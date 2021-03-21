8b25eda4dc4e0fd197811f2a5cece8ae79eaa6c2
acartoon <toontown@inbox.ru>
2021-03-21 11:37:44 +0300

    аfirst commit

---
+++ build/js/script.js
@@ -0,0 +1,13 @@
+
+$('.js-nav').mouseenter(function () {
+    $('.main-menu').addClass('open');
+
+    // var height = $(this).find($('.main-nav-dropdown__wrap')).innerHeight();
+    // var newHeight = height > 320 ? height : 320;
+    // $('.main-nav-dropdown').height(newHeight);
+
+});
+
+$('.js-nav').mouseleave(function () {
+    $('.main-menu').removeClass('open')
+});
